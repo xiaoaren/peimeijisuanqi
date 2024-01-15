e0af4ca7ef05325c615f4613b49bbf9b87c1ee5c
jitong` <1023466819@qq.com>
2024-01-15 22:34:24 +0800

    INIT

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "umi";
-const {PUBLIC_PATH ='/peimeijisuanqi'} = process.env
+const {PUBLIC_PATH =''} = process.env
 export default defineConfig({
-  base: PUBLIC_PATH, //要在非根目录下部署 umi 项目时，你可以使用 base 配置。 默认值：/
+  base: "/" + PUBLIC_PATH, //要在非根目录下部署 umi 项目时，你可以使用 base 配置。 默认值：/
   publicPath: PUBLIC_PATH + '/', //配置 webpack 的 publicPath。
   outputPath: 'docs', // 配置输出路径。  默认dist
   routes: [
