5c0173e9fd2da8eec501a045f8630c04cf7d1f84
jtzhang <1023466819@qq.com>
2024-01-15 18:47:22 +0800

    提交

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "umi";
 const {PUBLIC_PATH ='/peimeijisuanqi'} = process.env
 export default defineConfig({
-  // base: PUBLIC_PATH,
-  publicPath: PUBLIC_PATH + '/',
-  outputPath: 'docs', //默认dist
+  base: PUBLIC_PATH, //要在非根目录下部署 umi 项目时，你可以使用 base 配置。 默认值：/
+  publicPath: PUBLIC_PATH + '/', //配置 webpack 的 publicPath。
+  outputPath: 'docs', // 配置输出路径。  默认dist
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
